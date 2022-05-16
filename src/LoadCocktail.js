67a970185a48bba75b1e69c35cea106298686340
mikhailyatsenko <misha.ggg@gmail.com>
2022-05-16 23:11:31 +0300

    endless load- first edition

--- src/LoadCocktail.js
+++ src/LoadCocktail.js
@@ -2,18 +2,13 @@ import Cocktail from "./Cocktail";
 import React from "react";
 
 class LoadCocktail extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      cocktailName: "",
-      cocktailImage: "",
-      cocktailIngridients: [],
-      cocktailRecipe: "",
-      defaultHide: "hide",
-      defaultShow: "",
-      hideShowSpinner: "hide",
-    };
-  }
+  state = {
+    cocktailData: [],
+    defaultHide: "hide",
+    defaultShow: "",
+    hideShowSpinner: "hide",
+    buttonStatus: "",
+  };
 
   clickHandler = async (event) => {
     event.preventDefault();
@@ -22,35 +17,27 @@ class LoadCocktail extends React.Component {
     if (!t.classList.contains("load-cocktail")) return true;
 
     this.setState({
+      buttonStatus: "disabled",
       hideShowSpinner: "",
       defaultShow: "hide",
-      defaultHide: "hide",
     });
 
     let url = "https://cocktail.mikhailiatsien1.repl.co/random";
     let response = await fetch(url);
     let cocktailData = await response.json();
-    console.log(cocktailData);
-    // let keysArray = Object.keys(cocktailData);
-    // console.log(keysArray);
-    let ingrArr = [];
+    cocktailData.ingridients = [];
     for (let i = 1; i <= 15; i++) {
       let ingr = "strIngredient" + i;
-      ingrArr.push(cocktailData[ingr]);
+      if (cocktailData[ingr] !== null && cocktailData[ingr] !== "") {
+        cocktailData.ingridients.push(cocktailData[ingr]);
+      }
     }
-
-    console.log(ingrArr);
-
-    let ingridients = ingrArr.filter((item) => item !== null && item !== "");
-    console.log(ingridients);
 
     this.setState({
       hideShowSpinner: "hide",
-      cocktailName: cocktailData.strDrink,
-      cocktailImage: cocktailData.strDrinkThumb,
-      cocktailIngridients: ingridients,
-      cocktailRecipe: cocktailData.strInstructions,
+      cocktailData: [...this.state.cocktailData, cocktailData],
       defaultHide: "",
+      buttonStatus: "",
     });
   };
 
@@ -62,14 +49,11 @@ class LoadCocktail extends React.Component {
         className="bg-color-main height100 d-flex align-items-center"
       >
         <Cocktail
-          name={this.state.cocktailName}
-          image={this.state.cocktailImage}
-          ingridients={this.state.cocktailIngridients}
-          recipe={this.state.cocktailRecipe}
-          hideClass={this.state.hideClass}
+          cocktailData={this.state.cocktailData}
           defaultHide={this.state.defaultHide}
           defaultShow={this.state.defaultShow}
           hideShowSpinner={this.state.hideShowSpinner}
+          buttonStatus={this.state.buttonStatus}
         />
       </section>
     );
